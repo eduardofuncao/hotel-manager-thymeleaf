8fb657b5323807e6dbea92bdab0a600370c589bc
Eduardo Função <eduardofuncao@hotmail.com>
2025-03-05 01:13:16 -0300

    initial commit

---
+++ src/test/java/br/com/fiap/hotel_manager/HotelManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.hotel_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
